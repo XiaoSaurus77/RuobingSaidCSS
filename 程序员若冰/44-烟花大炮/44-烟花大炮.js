52df911b8ccacc8c775ec991c385d8824d5181ef
若冰 <464226879@qq.com>
2024-08-22 03:46:24 +0800

    Version: V2.4.6 Date: 2024-08-22 Centent: 程序员若冰-44-烟花大炮

---
+++ 程序员若冰/44-烟花大炮/44-烟花大炮.js
@@ -0,0 +1,24 @@
+/*** Uncomment the code below to get a "LAUNCH ALL" button ***/
+/*** Challenge proposed by PX, see the comments in Detail View ***/
+
+/*
+var x = document.createElement("button"); 
+document.body.appendChild(x); 
+function dqs(x){return document.querySelector(x);} 
+dqs('button').setAttribute('id', 'launch-all'); 
+dqs('button').innerText='LAUNCH ALL'; 
+dqs('#launch-all').addEventListener('click', function() { 
+	setTimeout(() => { dqs('#check-0').checked = true; }, 100); 
+	setTimeout(() => { dqs('#check-1').checked = true; }, 300); 
+	setTimeout(() => { dqs('#check-2').checked = true; }, 500); 
+	setTimeout(() => { dqs('#check-3').checked = true; }, 700); 
+	setTimeout(() => { dqs('#check-4').checked = true; }, 900); 
+	setTimeout(() => { dqs('#check-5').checked = true; }, 1100); 
+	setTimeout(() => { dqs('#check-6').checked = true; }, 1200); 
+	setTimeout(() => { dqs('#check-7').checked = true; }, 1000); 
+	setTimeout(() => { dqs('#check-8').checked = true; }, 800); 
+	setTimeout(() => { dqs('#check-9').checked = true; }, 600); 
+	setTimeout(() => { dqs('#check-10').checked = true; }, 400); 
+	setTimeout(() => { dqs('#check-11').checked = true; }, 200); 
+});
+*/
